c1516a017a063454a35d0d874f1575ba9c76f269
tkdgh7063 <hoo412298@gmail.com>
2025-04-15 18:51:30 +0900

    ThemeProvider practice

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,83 +6,17 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-`;
-
-const Text = styled.span`
-  color: white;
-  font-weight: 400;
-`;
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 1.5s linear infinite;
-  ${Text} {
-    &:hover {
-      font-weight: 800;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Btn = styled.button`
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-  color: white;
-`;
-
-const Input = styled.input.attrs({
-  required: true,
-  minLength: 4,
-  maxLength: 8,
-})`
-  background-color: turquoise;
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="teal">
-        <Text>HI IM BOX</Text>
-      </Box>
-      <Circle bgColor="tomato">
-        <span>HI IM CIRCLE</span>
-      </Circle>
-      <Btn>Log In</Btn>
-      <Btn as="a" href="/">
-        Log In
-      </Btn>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <Title>HELLO!!</Title>
     </Wrapper>
   );
 }
