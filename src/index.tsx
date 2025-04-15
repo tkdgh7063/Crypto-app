5c1b7297f48382dc101d148bb774c5918582a502
tkdgh7063 <hoo412298@gmail.com>
2025-04-15 19:36:33 +0900

    Migrate project to TypeScript
    - Renamed .js files to .tsx
    - Added tsconfig.json
    - Added styled.d.ts for theme typings
    - Fixed type errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, DefaultTheme } from "styled-components";
 import App from "./App";
 
-const darkTheme = {
+const darkTheme: DefaultTheme = {
   textColor: "whitesmoke",
   backgroundColor: "#111",
 };
 
-const lightTheme = {
+const lightTheme: DefaultTheme = {
   textColor: "#111",
   backgroundColor: "whitesmoke",
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={darkTheme}>
