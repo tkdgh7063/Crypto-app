77dea5b3a0d7aacc61b5d08207404104574743cd
tkdgh7063 <hoo412298@gmail.com>
2025-04-16 01:57:15 +0900

    TypeScript ThemeProvider practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
-import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState<string>("");
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setValue(value);
-  };
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-      <span>Welcome {value}</span>
-    </div>
+    <Container>
+      <H1>TITLE</H1>
+    </Container>
   );
 }
 
