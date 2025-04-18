49647cd2d9aff05b2164875af1119c4a0b08a5bc
tkdgh7063 <hoo412298@gmail.com>
2025-04-19 01:06:10 +0900

    Build coin detail UI with minor fixes

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { JSX, useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { styled } from "styled-components";
 
@@ -6,6 +6,9 @@ const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
   margin: 0 auto;
+  header + div {
+    margin-bottom: 12px;
+  }
 `;
 
 const Header = styled.header`
@@ -23,6 +26,81 @@ const Loader = styled.div`
   display: block;
   text-align: center;
 `;
+
+const Overview = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  padding: 10px 20px;
+  margin-bottom: 25px;
+`;
+
+const OverviewTitle = styled.div`
+  font-size: 22px;
+  font-weight: 600;
+  margin: -18px 0px 20px;
+  text-transform: uppercase;
+`;
+
+const OverviewItemWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  width: 110%;
+  gap: 20px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  span:first-child {
+    font-size: 16px;
+    font-weight: 800;
+    text-transform: uppercase;
+    margin-bottom: 10px;
+  }
+  span:nth-child(2) {
+    font-weight: 400;
+    font-size: 14px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 15px 0px 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  line-height: 1.8;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Logo = styled.img`
+  width: 45px;
+  height: 45px;
+`;
+
+const LinkWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  div:first-child {
+    font-weight: 600;
+    margin-bottom: 5px;
+  }
+`;
+
+const LinkIcon = styled.div``;
+
+const Link = styled.a``;
 
 interface RouteParams {
   coinId: string;
@@ -43,6 +121,20 @@ interface ITeam {
   name: string;
   position: string;
 }
+
+const linkCategories = [
+  "explorer",
+  "facebook",
+  "reddit",
+  "source_code",
+  "website",
+  "youtube",
+  "medium",
+] as const;
+
+// TODO: add link icons for each categories
+// [github, explorer(magnify), website(global | home)]
+const linkIcons = [""] as const;
 
 interface ILinks {
   explorer?: string[] | null;
@@ -76,12 +168,12 @@ interface IInfoData {
   started_at: string;
   development_status: string;
   hardware_wallet: boolean;
-  proof_type: string;
-  org_structure: string;
-  hash_algorithm: string;
+  proof_type?: string | null;
+  org_structure?: string | null;
+  hash_algorithm?: string | null;
   first_data_at: string;
   last_data_at: string;
-  links: ILinks[];
+  links: ILinks;
   whitepaper: IWhitepaper[];
 }
 
@@ -120,13 +212,14 @@ interface IPriceData {
 }
 
 function Coin() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
 
   const [info, setInfo] = useState<IInfoData>();
   const [priceInfo, setPriceInfo] = useState<IPriceData>();
 
+  // TODO: use Promise.all() on fetch 2 urls for efficiency
   useEffect(() => {
     (async () => {
       const infoData = await (
@@ -139,15 +232,156 @@ function Coin() {
 
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })();
-  }, []);
+  }, [coinId]);
 
   return (
     <Container>
       <Header>
-        <Title>{state?.name || "Loading..."}</Title>
+        <Title>{state?.name ? state.name : "Loading..."}</Title>
       </Header>
-      {loading ? <Loader>Loading...</Loader> : <h1>HI!!</h1>}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewTitle>Basic</OverviewTitle>
+            <OverviewItemWrapper>
+              <OverviewItem>
+                <span>rank</span>
+                <span>{info?.rank}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>symbol</span>
+                <span>${info?.symbol}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>type</span>
+                <span>{info?.type.toUpperCase()}</span>
+              </OverviewItem>
+            </OverviewItemWrapper>
+          </Overview>
+          <LogoContainer>
+            <Logo src={info?.logo} />
+          </LogoContainer>
+          <Description>
+            {info?.description === "" || undefined
+              ? "No Description"
+              : info?.description}
+          </Description>
+          <Overview>
+            {/* TODO: replace O, X signs to icons later */}
+            <OverviewTitle>Status</OverviewTitle>
+            <OverviewItemWrapper>
+              <OverviewItem>
+                <span>is_active</span>
+                <span>{info?.is_active ? "O" : "X"}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>is_new</span>
+                <span>{info?.is_new ? "O" : "X"}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>open_source</span>
+                <span>{info?.open_source ? "O" : "X"}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>hardware_wallet</span>
+                <span>{info?.hardware_wallet ? "O" : "X"}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>message</span>
+                <span>
+                  {info?.message === "" || undefined ? "None" : info?.message}
+                </span>
+              </OverviewItem>
+            </OverviewItemWrapper>
+          </Overview>
+          <Overview>
+            <OverviewTitle>Technology</OverviewTitle>
+            <OverviewItemWrapper>
+              <OverviewItem>
+                <span>development_status</span>
+                <span>
+                  {info?.development_status ? info?.development_status : "None"}
+                </span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>proof_type</span>
+                <span>{info?.proof_type ? info?.proof_type : "None"}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>org_structure</span>
+                <span>
+                  {info?.org_structure ? info?.org_structure : "None"}
+                </span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>hash_algorithm</span>
+                <span>
+                  {info?.hash_algorithm ? info?.hash_algorithm : "None"}
+                </span>
+              </OverviewItem>
+            </OverviewItemWrapper>
+          </Overview>
+          <Overview>
+            <OverviewTitle>Team Info</OverviewTitle>
+            <OverviewItemWrapper>
+              {info?.team.map((t) => (
+                <OverviewItem>{t.name}</OverviewItem>
+              ))}
+            </OverviewItemWrapper>
+          </Overview>
+          <Overview>
+            {/* TODO: align numbers in same columns to look better later */}
+            <OverviewTitle>Tag Info</OverviewTitle>
+            <OverviewItemWrapper>
+              {info?.tags.map((t) => (
+                <OverviewItemWrapper>
+                  <OverviewItem>{t.name}</OverviewItem>
+                  <OverviewItem>{t.coin_counter}</OverviewItem>
+                  <OverviewItem>{t.ico_counter}</OverviewItem>
+                </OverviewItemWrapper>
+              ))}
+            </OverviewItemWrapper>
+          </Overview>
+          <Overview>
+            <OverviewTitle>Links</OverviewTitle>
+            <OverviewItemWrapper>
+              {linkCategories.reduce<JSX.Element[]>((acc, category) => {
+                const urls = info?.links[category];
+                if (urls && urls.length > 0) {
+                  acc.push(
+                    <LinkWrapper key={category}>
+                      <OverviewItem>{category.toUpperCase()}</OverviewItem>
+                      {urls.map((url) => (
+                        <Link href={url} target="_blank">
+                          {category} {/* replace category with icon later */}
+                        </Link>
+                      ))}
+                    </LinkWrapper>
+                  );
+                }
+                return acc;
+              }, [])}
+            </OverviewItemWrapper>
+          </Overview>
+          <Overview>
+            <OverviewTitle>Ticker</OverviewTitle>
+            <OverviewItemWrapper>
+              <OverviewItem>
+                <span>total supply</span>
+                <span>{priceInfo?.total_supply}</span>
+              </OverviewItem>
+              <OverviewItem>
+                <span>max supply</span>
+                <span>{priceInfo?.max_supply}</span>
+              </OverviewItem>
+            </OverviewItemWrapper>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 }
